8207f8706733351444b3513788bf73239c61ada3
Kunja Salamanca <kunjabihari20@Gmail.com>
2024-11-04 15:33:57 +0800

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Provider as PaperProvider } from 'react-native-paper';
+import LoginPage from './components/loginFrame';
+import RegistrationPage from './components/registrationFrame';
+import PasswordRecoveryPage from './components/recoveryFrame';
 
-export default function App() {
+const App = () => {
+  const [currentPage, setCurrentPage] = useState('login');
+
+  const renderPage = () => {
+    switch (currentPage) {
+      case 'login':
+        return <LoginPage onSwitch={() => setCurrentPage('recovery')} onRegister={() => setCurrentPage('register')} />;
+      case 'register':
+        return <RegistrationPage onSwitch={() => setCurrentPage('login')} />;
+      case 'recovery':
+        return <PasswordRecoveryPage onSwitch={() => setCurrentPage('login')} />;
+      default:
+        return <LoginPage onSwitch={() => setCurrentPage('recovery')} onRegister={() => setCurrentPage('register')} />;
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      {renderPage()}
+    </PaperProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
